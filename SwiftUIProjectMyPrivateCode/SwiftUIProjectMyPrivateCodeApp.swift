41c56f12e7b1c3a1e31e198d6e58e7ae7b79d45c
Mohammad <ma@bornak.de>
2025-04-11 17:12:39 +0330

    Initial Commit

---
+++ SwiftUIProjectMyPrivateCode/SwiftUIProjectMyPrivateCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectMyPrivateCodeApp.swift
+//  SwiftUIProjectMyPrivateCode
+//
+//  Created by Mohammad on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectMyPrivateCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
